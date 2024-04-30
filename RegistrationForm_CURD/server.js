ff4015901d7cf9fb8bd33d0c10e0f879e76894a8
Vishwesh2241 <vishwesh2311@gmail.com>
2024-04-30 14:37:08 +0530

    registration

--- RegistrationForm_CURD/server.js
+++ RegistrationForm_CURD/server.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -23,6 +22,15 @@ db.connect((err)=>{
         console.log('Connected to database');
     }
 });
+
+app.use(session({
+    secret: 'your_secret_key',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     res.redirect('/login');
@@ -45,9 +53,63 @@ app.post('/register', (req, res) => {
 
     db.query(query,[username,password], (err)=>{
         if(err) throw err;
-        res.redirect('/login');
+        if (results.length > 0) {
+            req.session.username = username;
+            res.redirect('/home');
+        } else {
+            res.redirect('/login');
+        }
     });
 })
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+
+    db.query(query, [username, password], (err, results) => {
+        if (err) throw err;
+
+        if (results.length > 0) {
+            req.session.username = username;
+            res.redirect('/home');
+        } else {
+            res.redirect('/login');
+        }
+    });
+});
+
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
+
+    db.query(query, [username, password], (err) => {
+        if (err) throw err;
+        res.redirect('/login');
+    });
+});
+
+app.get('/home', (req, res) => {
+    if (req.session.username) {
+        res.render('home.ejs', { username: req.session.username });
+    } else {
+        res.redirect('/login');
+    }
+});
+app.get('/home', (req, res) => {
+    if (req.session.username) {
+        res.render('home.ejs', { username: req.session.username });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+});
+
+
 
 app.listen(1001,()=>{
     console.log('Server is running on port 1001');
